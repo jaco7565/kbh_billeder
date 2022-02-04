f9079b85c5f57ed96f7928f6f646d9da87e0a07b
jaco7565 <jaco7565@stud.kea.dk>
2022-02-04 07:00:49 +0100

    last commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F0ECE3",
+        kasse: "#DFD3C3",
+        knap: "#F58840",
+        hover: "#F8AC79",
+      },
+      fontFamily: {
+        stix: "STIX Two Text, serif",
+        underskrift: "PT Sans Narrow', sans-serif",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
